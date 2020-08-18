7b08aac47b100b685346f6fda795fd00edef1833
Priscila Netto <priscila.nnetto@hotmail.com>
2020-08-18 17:02:28 -0300

    validação do form com js

---
+++ scripts.js
@@ -0,0 +1,95 @@
+
+// function mensagemEnvio() {
+//   window.location.href = "mensagem.html"
+// }
+
+const fields = document.querySelectorAll("[required]");
+
+function validateField(field) {
+
+  function verifyErrors() {
+    let foundError = false;
+
+    for (let error in field.validity) {
+
+
+      if (field.validity[error] && !field.validity.valid) {
+        foundError = error;
+      }
+    }
+
+
+    return foundError
+
+  }
+
+  function customMessage(typeError) {
+    const messages = {
+      text: {
+        valueMissing: "Por favor, preencha este campo"
+      },
+      email: {
+        valueMissing: "Email é obrigatório",
+        typeMismatch: "Por favor, preencha um email válido"
+      },
+      textarea: {
+        valueMissing: "Por favor, preencha este campo"
+      }
+
+    }
+    return messages[field.type][typeError]
+  }
+
+  function setCustomMessage(message) {
+    const spanError = field.parentNode.querySelector("span.error")
+
+    if (message) {
+      spanError.classList.add("active")
+      spanError.innerHTML = message
+    } else {
+      spanError.classList.remove("active")
+      spanError.innerHTML = ""
+    }
+
+  }
+
+  return function () {
+    const error = verifyErrors();
+
+    if (error) {
+      const message = customMessage(error)
+
+      setCustomMessage(message)
+    } else {
+
+      setCustomMessage()
+    }
+  }
+
+}
+
+
+function customValidation(event) {
+
+  const field = event.target
+  const validation = validateField(field)
+
+  validation()
+
+
+}
+
+
+for (field of fields) {
+  field.addEventListener("invalid", event => {
+    event.preventDefault()
+    customValidation(event)
+  })
+  field.addEventListener("blur", customValidation)
+}
+
+
+document.querySelector("form").addEventListener("submit", event => {
+  console.log("Pode enviar o formulário");
+  event.preventDefault()
+})
